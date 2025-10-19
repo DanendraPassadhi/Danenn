47f892442270524422a696552d803ee14041523e
Danendra Nayaka Passadhi <danendra.passadhi71@gmail.com>
2025-10-19 13:53:35 +0700

    initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+
 export default defineConfig({
-  base: '/Danenn/', 
-  plugins: [react()],
+  base: '/Danenn/', 
+  plugins: [react()],
 })
